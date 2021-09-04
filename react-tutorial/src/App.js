41166c240ed09fbd85023caab2f25e336cdc09ab
Bryan Taylor <mcbtay@gmail.com>
2021-09-04 17:52:52 -0400

    inital commit of the tutorial project from react js website.  will use this to work through the tutorial

---
+++ react-tutorial/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <ShoppingList name="Bryan" />
+        <ShoppingList name="Sierra" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
