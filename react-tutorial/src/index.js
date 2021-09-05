937e783052e1cd4ead89e1da5cd47f49b0dd7e79
Bryan Taylor <mcbtay@gmail.com>
2021-09-04 21:45:11 -0400

    bolding selected move, working through suggestions at end of tutorial

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -89,7 +89,12 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      let desc = move ? 'Go to move #' + move : 'Go to game start';
+
+      if (move === this.state.stepNumber) {
+        desc = <b>{desc}</b>;
+      }
+
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
