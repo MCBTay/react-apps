b3f799e9a36ff0d8f93909a0c641e0fcf211a94a
Bryan Taylor <mcbtay@gmail.com>
2021-09-08 00:06:50 -0400

    updates to support gh-pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import './index.css'
 
@@ -190,7 +191,9 @@ function calculateWinner(squares) {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Game />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
